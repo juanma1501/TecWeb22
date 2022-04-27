b33c06012308d77dca528637891780817037538d
juanma1501 <juanmaporreroalmansa@gmail.com>
2022-04-27 18:40:19 +0200

    All classes related to chess were deleted.

--- src/main/java/edu/uclm/esi/tys2122/dao/MovementRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.uclm.esi.tys2122.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.uclm.esi.tys2122.newchess.ChessMovement;
-
-public interface MovementRepository extends JpaRepository <ChessMovement, String> {
-
-}
